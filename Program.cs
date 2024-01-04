4b07c1c0908f1421a56d577043f8625b279ab87a
Brandoneius <brandmthomas@yahoo.com>
2024-01-04 15:41:32 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -104,3 +104,24 @@ foreach (string fraud in fraudulentOrderIDs)
 {
     Console.WriteLine(fraud);
 }
+
+//For Loop Exercise:
+string[] orders =
+{
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+};
+
+foreach (string order in orders)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
+}
